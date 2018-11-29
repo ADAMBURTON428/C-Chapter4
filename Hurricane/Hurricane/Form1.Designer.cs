574e580d736bdc20cfacc7c42254aded44a6acc2
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-11-29 11:53:35 -0600

    update

---
+++ Hurricane/Hurricane/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Hurricane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWindSpeed = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblWindCategory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Wind Speed";
+            // 
+            // txtWindSpeed
+            // 
+            this.txtWindSpeed.Location = new System.Drawing.Point(223, 97);
+            this.txtWindSpeed.Name = "txtWindSpeed";
+            this.txtWindSpeed.Size = new System.Drawing.Size(100, 20);
+            this.txtWindSpeed.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(203, 144);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWindCategory
+            // 
+            this.lblWindCategory.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWindCategory.Location = new System.Drawing.Point(191, 208);
+            this.lblWindCategory.Name = "lblWindCategory";
+            this.lblWindCategory.Size = new System.Drawing.Size(100, 23);
+            this.lblWindCategory.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWindCategory);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtWindSpeed);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWindSpeed;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblWindCategory;
+    }
+}
+
---
+++ Hurricane/Hurricane/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hurricane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int WindSpeed;
+            const int Category5 = 157;
+            const int Category4 = 130;
+            const int Category3 = 111;
+            const int Category2 = 96;
+            const int Category1 = 75;
+
+            WindSpeed = Convert.ToInt32(txtWindSpeed.Text);   
+
+            if (WindSpeed < Category1)
+            {
+                lblWindCategory.Text = "Not a Hurricane";
+            }
+            else if (WindSpeed > Category1 && WindSpeed < Category2)
+            {
+                lblWindCategory.Text = "Category 1";
+            }
+            esle if (WindSpeed)
+        }
+
+      
+    }
+}
